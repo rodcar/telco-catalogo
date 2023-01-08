2620e7de34338096c85b439efc0bd4ce2d65ff67
rodcar <ivanrod12@hotmail.com>
2023-01-08 00:24:33 -0500

    commit inicial

---
+++ src/main/java/pe/telco/catalogo/TelcoCatalogoApplication.java
@@ -0,0 +1,13 @@
+package pe.telco.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/telco/catalogo/TelcoCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.telco.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
