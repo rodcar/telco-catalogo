751c8389349b6ce6948c35a67547784341364c41
rodcar <ivanrod12@hotmail.com>
2023-01-11 20:14:55 -0500

    agrega Circuit Breaker para paquetes de redes sociales

--- src/main/java/pe/telco/catalogo/services/CatalogoServiceImpl.java
+++ src/main/java/pe/telco/catalogo/services/CatalogoServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,6 +21,12 @@ public class CatalogoServiceImpl implements CatalogoService {
 	@Autowired
 	private WebClient.Builder webClientBuilder;
 
+	private final ReactiveCircuitBreaker catalogoCircuitBreaker;
+
+	public CatalogoServiceImpl(ReactiveCircuitBreakerFactory circuitBreakerFactory) {
+		this.catalogoCircuitBreaker = circuitBreakerFactory.create("recommended");
+	}
+
 	@Override
 	public List<CatalogoItem> fetchAllItems() throws Exception {
 		List<CatalogoItem> items = new ArrayList<>();
@@ -33,7 +41,7 @@ public class CatalogoServiceImpl implements CatalogoService {
 			items.add(CatalogoItem.builder().id(paquete.getId().toString()).nombre(paquete.getNombre())
 					.descripcion(paquete.getDescripcion()).precio(paquete.getPrecio()).categoria("redes").build());
 		}
-		
+
 		Mono<List<PaqueteInternetResponse>> internetPaqueteResponse = webClientBuilder.build().get()
 				.uri("http://paquetesinternet/paquetesinternet").retrieve()
 				.bodyToMono(new ParameterizedTypeReference<List<PaqueteInternetResponse>>() {
@@ -54,10 +62,15 @@ public class CatalogoServiceImpl implements CatalogoService {
 
 		switch (categoria) {
 		case "redes":
-			Mono<List<PaqueteResponse>> redSocialPaqueteResponse = webClientBuilder.build().get()
-					.uri("http://paquetes/paquetes").retrieve()
-					.bodyToMono(new ParameterizedTypeReference<List<PaqueteResponse>>() {
-					});
+			Mono<List<PaqueteResponse>> redSocialPaqueteResponse = catalogoCircuitBreaker
+					.run(webClientBuilder.build().get().uri("http://paquetes/paquetes").retrieve()
+							.bodyToMono(new ParameterizedTypeReference<List<PaqueteResponse>>() {
+							}), throwable -> {
+								List<PaqueteResponse> predeterminada = new ArrayList<>();
+								// TODO Agregar paquetes predeterminados
+								// TODO Retornar una Excepción personaliza
+								return Mono.just(predeterminada);
+							});
 
 			List<PaqueteResponse> paquetes = redSocialPaqueteResponse.block();
 
@@ -75,7 +88,8 @@ public class CatalogoServiceImpl implements CatalogoService {
 			List<PaqueteInternetResponse> paquetesInternet = internetPaqueteResponse.block();
 			for (PaqueteInternetResponse paquete : paquetesInternet) {
 				items.add(CatalogoItem.builder().id(paquete.getId()).nombre(paquete.getNombre())
-						.descripcion(paquete.getDescripcion()).precio(paquete.getPrecio()).categoria("internet").build());
+						.descripcion(paquete.getDescripcion()).precio(paquete.getPrecio()).categoria("internet")
+						.build());
 			}
 			break;
 		default:
